171f9a3790d243788e517a85fe239253456d8c46
fred su <ck.fred.su@gmail.com>
2020-04-22 23:02:55 +0800

    guess_01

---
+++ guess.py
@@ -0,0 +1,21 @@
+#產生一個隨機整數1~100(不要印出來)
+#讓使用者重複輸入數字去猜
+#猜對的話 印出"終於猜對了"
+#猜錯的話 要告訴他比答案大還是答案小
+
+
+import random
+
+r = random.randint(1,100)
+while True :
+	ans = input('請輸入數字:')
+	ans = int(ans)
+	if ans == r :
+		print('終於猜對了')
+		break
+	elif ans < r :
+		print('你猜的數字比答案小')
+	elif ans > r :
+		print('你猜的數字比答案大')
+	elif ans == null :
+		print('輸入有問題')
